3487ed0dca269d353e5444815ca7d0821e8103b6
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-09-27 00:16:32 +0200

    prueba con kubernetes

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,3 +1,3 @@
-export const events: string ="http://events";
-export const booking: string = "http://bookings";
+export const events: string ="http://api.myevents.example";
+export const booking: string = "http://api.myevents.example/bookings";
 
